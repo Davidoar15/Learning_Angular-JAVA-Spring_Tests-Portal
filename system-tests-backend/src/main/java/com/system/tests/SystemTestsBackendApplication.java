8a12ddfc78ae18c2c3b6a082f0f7028401ab89eb
Davidoar15 <davidoar15@gmail.com>
2024-01-17 13:55:51 -0300

    initial_commit_back

---
+++ system-tests-backend/src/main/java/com/system/tests/SystemTestsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.system.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemTestsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemTestsBackendApplication.class, args);
+	}
+
+}
---
+++ system-tests-backend/src/test/java/com/system/tests/SystemTestsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemTestsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

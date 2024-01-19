ce4890cbffbe66d05d018b8c97929d79d1d4fe58
Davidoar15 <davidoar15@gmail.com>
2024-01-19 19:08:14 -0300

    add/form_&_validation_signup

--- system-tests-backend/src/main/java/com/system/tests/controllers/UserController.java
+++ system-tests-backend/src/main/java/com/system/tests/controllers/UserController.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("*")
 public class UserController {
     
     @Autowired
@@ -27,6 +29,7 @@ public class UserController {
 
     @PostMapping("/")
     public User saveUser(@RequestBody User user) throws Exception {
+        user.setProfile("default.png");
         Set<UserRol> roles = new HashSet<>();
 
         Rol rol = new Rol();
--- system-tests-frontend/src/app/pages/signup/signup.component.ts
+++ system-tests-frontend/src/app/pages/signup/signup.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input"
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatFormFieldModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private _userService: UserService) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      cellPhone: ['', Validators.required]
+    });
+  };
+
+  formSubmit() {
+    const user: any = {
+      username: this.form.value.username,
+      password: this.form.value.password,
+      name: this.form.value.name,
+      lastName: this.form.value.lastName,
+      email: this.form.value.email,
+      cellPhone: this.form.value.cellPhone,
+    }
+    console.log(user)
+
+    this._userService.addUser(user).subscribe((data) => {
+      console.log(data);
+      alert("User resgistered successfully");
+    }, (error) => {
+      console.log(error);
+      alert("ERROR");
+    });
+  }
 }

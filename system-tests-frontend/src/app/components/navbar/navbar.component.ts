247308e7f59cb54d45443dc3760228dc06b06b7b
Davidoar15 <davidoar15@gmail.com>
2024-01-19 21:59:14 -0300

    add/navbar_&_routing

--- system-tests-frontend/src/app/components/navbar/navbar.component.ts
+++ system-tests-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatToolbarModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })

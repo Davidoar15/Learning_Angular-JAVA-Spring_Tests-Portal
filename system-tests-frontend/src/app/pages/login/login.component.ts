7e878c3d7d1fce018be57d2358154576c04fa4dc
Davidoar15 <davidoar15@gmail.com>
2024-01-19 21:14:22 -0300

    add/initial_login_&_home

--- system-tests-frontend/src/app/pages/login/login.component.ts
+++ system-tests-frontend/src/app/pages/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
